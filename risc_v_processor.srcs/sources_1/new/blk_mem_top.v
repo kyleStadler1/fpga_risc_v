55a02ee44a3d21b52722b798ae5ab04917a98461
kyleStadler1 <kcs-github@stadfam.net>
2024-07-15 00:33:34 -0700

    add mem interface

---
+++ risc_v_processor.srcs/sources_1/new/blk_mem_top.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/15/2024 12:25:56 AM
+// Design Name: 
+// Module Name: blk_mem_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module blk_mem_top(
+    input clk,
+    input ren,
+    input [14:0] addr,
+    input [31:0] din,
+    output [31:0] dout,
+    output reg read_valid
+    );
+    blk_mem_gen M(clk, {ren,ren,ren,ren}, addr, din, dout);
+    reg [15:0] prev_op;
+    always @(posedge clk) begin
+        prev_op <= {addr, ren};
+        read_valid <= prev_op == {addr, ren};
+   end 
+endmodule
+
