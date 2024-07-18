459409a7f44142c2ad5082606742940453efd3a7
kyleStadler1 <kcs-github@stadfam.net>
2024-07-17 22:05:01 -0700

    create alu module

---
+++ risc_v_processor.srcs/sources_1/new/alu.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/17/2024 10:03:06 PM
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+
+    );
+endmodule
