4bc90b44142d4231fc8d24521de3b7dbc10f3f04
kyleStadler1 <kcs-github@stadfam.net>
2024-07-21 23:05:17 -0700

    instrwriter works, execute_tb instructions never emerge

--- risc_v_processor.srcs/sim/tb_fetch.v
+++ risc_v_processor.srcs/sim/tb_fetch.v
@@ -7,9 +7,9 @@ module tb_fetch;
     reg [31:0] branch_vect;
     reg branch_en;
 
-    reg dma_instr_en;
-    reg [31:0] dma_instr_addr;
-    reg [31:0] dma_instr_data;
+    wire dma_instr_en;
+    wire [31:0] dma_instr_addr;
+    wire [31:0] dma_instr_data;
 
     wire [31:0] instruction;
     wire instruction_valid;
@@ -28,15 +28,15 @@ module tb_fetch;
         .instr_valid(instruction_valid),
         .pc_val(pc_val)
     );
+    
+   instr_writer writer(
+        .clk(clk),
+        .instr_write_en(dma_instr_en),
+        .addr(dma_instr_addr),
+        .data(dma_instr_data)
+    );
 
 
-    reg [31:0] mi [0 : 2];
-    initial begin
-        dma_instr_en = 1;
-        mi[0] <= 32'h00600093;
-        mi[1] <= 32'h00700113;
-        mi[2] <= 32'h002081b3;
-    end
     always begin
         #20 clk <= clk + 1;
     end
@@ -48,44 +48,16 @@ module tb_fetch;
     end
     always @(*) begin
         case(ctr) 
-            0 : begin
-                dma_instr_en = 1;
-                dma_instr_addr = 32'd0;
-                dma_instr_data = mi[0];
-                branch_en = 0;
-            end
-            1 : begin
-                dma_instr_en = 1;
-                dma_instr_addr = 32'd4;
-                dma_instr_data = mi[1];
-                 branch_en = 0;
-            end
-            2 : begin
-                dma_instr_en = 1;
-                dma_instr_addr = 32'd8;
-                dma_instr_data = mi[2];
-                 branch_en = 0;
-            end
             3 : 
             begin
-                dma_instr_en = 0;
-                dma_instr_addr = 32'dx;
-                dma_instr_data = 32'dx;
                 branch_en = 0;
             end
             4 : begin
-                dma_instr_en = 0;
-                dma_instr_addr = 32'dx;
-                dma_instr_data = 32'dx;
                  branch_en = 0;
             end
             5 :  begin
-                dma_instr_en = 0;
-                dma_instr_addr = 32'dx;
-                dma_instr_data = 32'dx;
                 branch_en = 1;
                 branch_vect = 32'd0;
-                //ctr <= 3;
             end
             6 : begin
                 ctr <= 3;                
--- risc_v_processor.srcs/sources_1/new/instr_writer.v
+++ risc_v_processor.srcs/sources_1/new/instr_writer.v
@@ -10,26 +10,33 @@ module instr_writer (
 
 parameter last_index = 32'd2;
 
-reg [8:0] mi [0 : last_index];
+reg [31:0] mi [0 : last_index];
 
+reg _en;
+reg [31:0] _addr;
+reg [31:0] _data;
 
 reg init = 0;
 reg [31:0] load_index = 32'b0;
 always @(posedge clk) begin
     if (!init) begin
-        instr_write_en = 1;
+        _en = 1;
         mi[0] <= 32'h00600093;
         mi[1] <= 32'h00700113;
         mi[2] <= 32'h002081b3;
         init <= 1;
-    end else if (load <= last_index) begin
-        instr_write_en = 1;
-        addr = load_index << 2;
-        data = mi[load_index];
+    end else if (load_index <= last_index) begin
+        _en = 1;
+        _addr = load_index << 2;
+        _data = mi[load_index];
+        load_index <= load_index + 1;
     end else begin
-        instr_write_en = 0;
-        addr = 32'bx;
-        data = 32'bx;
+        _en = 0;
+        _addr = 32'bx;
+        _data = 32'bx;
     end
-end
+    end
+    assign instr_write_en = _en;
+    assign addr = _addr;
+    assign data = _data;
 endmodule
